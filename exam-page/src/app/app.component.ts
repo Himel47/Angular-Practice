b013af60d0c83050cd286a15f640f7e8a20e5afc
Md. Shahriar Kabir Himel <death44635@gmail.com>
2024-09-18 15:58:12 +0600

    login component done

--- exam-page/src/app/app.component.ts
+++ exam-page/src/app/app.component.ts
@@ -3,11 +3,20 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "../Components/header/header.component";
 import { FooterComponent } from "../Components/footer/footer.component";
 import { LoginComponent } from "../Components/login/login.component";
+import { SidebarComponent } from "../Components/sidebar/sidebar.component";
+import { NoticeComponent } from "../Components/notice/notice.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, LoginComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    SidebarComponent,
+    NoticeComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
